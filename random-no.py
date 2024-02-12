758460efefdc047af8e99dd703b6e299479c3e0a
SpandanNagale <spandan4844@gmail.com>
2024-02-13 02:26:17 +0530

    small changes

--- random-no.py
+++ random-no.py
@@ -1,13 +1,5 @@
-# a=input("enter two digit no.")
-# try:
-#     if(len(a)==2):
-#         print(a)
-# except IOError:
-#     print("wrong error")
+
 import random as r
-# a=[]
-# r.seed(3)
-# print(r.randint(0,1000))
 r.seed(3)
 a=r.randint(0,1000)
 print(a)
