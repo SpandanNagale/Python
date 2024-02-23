7bb4844e1b81d57f7b0f351ae7207ab43700724c
SpandanNagale <spandan4844@gmail.com>
2024-02-23 15:56:01 +0530

    library code is updated

--- Library.py
+++ Library.py
@@ -1,9 +1,15 @@
 class library :
     no_book=5
-    book=["maths, physics,c++,python,chemistry"]
+    book=["maths,physics,c++,python,chemistry"]
     
-    def add(a):
-        a="name of the book"
+    def add(self,a):
+       
         print(f"name of the book is {a}")
+        self.no_book=self.no_book+1
+        self.book.append(a)
+    def showlib(self):
+        print(f"the total no of book in library is {self.no_book} and library is {self.book}")    
 
-        
+a=library()
+a.add("game")
+a.showlib()
